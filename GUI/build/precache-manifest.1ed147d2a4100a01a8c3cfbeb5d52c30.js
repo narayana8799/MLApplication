f49b1b8ba9a9d5403a929a1a36783e4c6d32670c
shivam jadhav <jadhavshivam0228@gmail.com>
2020-09-27 23:44:27 +0530

    added layout

---
+++ GUI/build/precache-manifest.1ed147d2a4100a01a8c3cfbeb5d52c30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f32f79458c4d70764bee886b5dc672",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5555036abf8d25e707ac",
+    "url": "./static/css/main.813adc41.chunk.css"
+  },
+  {
+    "revision": "477a0cde54505075749f",
+    "url": "./static/js/2.343d7d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.343d7d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5555036abf8d25e707ac",
+    "url": "./static/js/main.4494ba29.chunk.js"
+  },
+  {
+    "revision": "ceea173e9dc92b9a0238",
+    "url": "./static/js/runtime-main.7c0942b2.js"
+  },
+  {
+    "revision": "fb1c4ff273936861ffbff9ac7437e3d9",
+    "url": "./static/media/i1.fb1c4ff2.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
